c585af5c11c12141bd62111da5c7507753a6476d
Emilio Ginzo <68378278+EmilioGinzo@users.noreply.github.com>
2022-06-10 16:54:44 -0400

    working on iterable for messages
    making one json for each coulumn in the excel

--- code/excel_to_json/verification_json_generator.py
+++ code/excel_to_json/verification_json_generator.py
@@ -1,24 +1,24 @@
 import pandas as pd
 import json
 import numpy as np 
+import os
 
-def get_json_xmlData(df):
-    values_json_path = "values/MeasurementSeriesNotification/values.json"
+def get_json_xmlData(row, values_json_path):
     values_json_file = open(values_json_path, "r")
     values_json_data = json.load(values_json_file)
-    time_series = values_json_data[df['values file name']['message_1']]
     values_json_file.close()
+    time_series = values_json_data[row['values file']]
     xml_data = {
-        "ExpectedErrorCode": df['message path']['message_1'],
-        "messageID": df['messageID']['message_1'],
-        "XMLName": "FALTA_NOMBRE",
-        "XMLFolderPath": "FALTA_RUTA",
+        "ExpectedErrorCode": row['error code'],
+        "messageID": row['Message ID'],
+        "XMLName": row['message name'],
+        "XMLFolderPath": row['message path'],
         "timeSerie": time_series,
-        "startDate": df['startDate']['message_1'],
-        "endDate": df['endDate']['message_1'],
-        "reciver": df['reciver']['message_1'],
-        "gridpoint": df['gridpoint']['message_1'],
-        "direction": df['direction']['message_1'],
+        "startDate": row['startDate'],
+        "endDate": row['endDate'],
+        "reciver": row['reciver'],
+        "gridpoint": row['grid_point'],
+        "direction": row['direction']
     }
     return xml_data
 
@@ -47,7 +47,7 @@ def get_sub_df_transposed_reindex(df: pd.DataFrame, start_index, end_index):
 def get_df_from_excel_base(excel_path):
     df = pd.read_excel(excel_path, na_filter = False)
     df_col1_list = df.iloc[:, 0].tolist()
-    start_json_info = df_col1_list.index("Info para json") + 1
+    start_json_info = df_col1_list.index("info para json") + 1
     end_json_info = len(df_col1_list)
     return get_sub_df_transposed(df, start_json_info, end_json_info)
 
@@ -64,7 +64,13 @@ def create_json_array(df):
         json_array.append(dict_data) 
     return json_array   
 
-def get_config_json_from_excel_EBASE_Struct(excel_path):
+def get_config_json_from_excel_EBASE_Struct(row, excel_path):
+    list_struct = [f for f in row.axes[0].tolist() if f.startswith('struct_')]
+    df = pd.read_excel(excel_path, na_filter = False)
+    #TODO: ayuda
+    return config_json
+
+def get_config_json_from_excel_EBASE_Struct1(row, excel_path):
     df = pd.read_excel(excel_path, na_filter = False)
     df_col1_list = df.iloc[:, 0].tolist()
     marketparties_index = 0
@@ -88,13 +94,24 @@ def get_config_json_from_excel_EBASE_Struct(excel_path):
     config_json['connections'] = create_json_array(connections_df)
 
     return config_json
-
-df = get_df_from_excel_base('excel/posible nuevo excel base.xlsx')
-json_xml_data = [get_json_xmlData(df)]
-json_config = get_config_json_from_excel_EBASE_Struct('excel/EBASE Struct.xlsx')
-validation_json = {
-    'config': json_config,
-    'xmlData': json_xml_data
-}
-with open('code/excel_to_json/test.json', 'w') as outfile:
-        json.dump(validation_json, outfile, indent=4)
+folder_path = 'excel/message_details'
+list_excels = [f for f in os.listdir(folder_path) if not f.startswith('~')]
+list_excels = [f for f in list_excels if f.endswith('.xlsx')]
+for excel_file in list_excels:
+    message_name = excel_file.rsplit('_',1)[1].split('.')[0]
+    excel_path = folder_path + '/' + excel_file
+    df_info_json = get_df_from_excel_base(excel_path)
+    values_json_path = "values/" + message_name + "/values.json"
+    #for para cada columna del json
+    for row in df_info_json.iterrows():
+        message_name = row[0]
+        if message_name != 'DEFAULT VALUES':
+            row = row[1]
+            json_xml_data = [get_json_xmlData(row, values_json_path)]
+            json_config = get_config_json_from_excel_EBASE_Struct(row, 'excel/EBASE Struct.xlsx')
+            validation_json = {
+                'config': json_config,
+                'xmlData': json_xml_data
+            }
+            with open('code/excel_to_json/test.json', 'w') as outfile:
+                    json.dump(validation_json, outfile, indent=4)
