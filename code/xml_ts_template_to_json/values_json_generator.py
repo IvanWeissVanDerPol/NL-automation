bf78b885cf6cbdbaf6d69cd05a9f3afb5799c060
Emilio Ginzo <68378278+EmilioGinzo@users.noreply.github.com>
2022-06-14 16:51:14 -0400

    now it saves the direction data in the json

--- code/xml_ts_template_to_json/values_json_generator.py
+++ code/xml_ts_template_to_json/values_json_generator.py
@@ -1,7 +1,8 @@
 import re
 import os
 import json
-import paths
+#import paths
+
 def values_json_generator(xml_path):
     """
         generates a json file with the values of the quantities in the xml files that contains the folder that is passed as a parameter
@@ -30,16 +31,22 @@ def values_json_generator(xml_path):
                 with open(file_path) as file:
                     file_name = os.path.basename(file_path)
                     quantity = []
+                    direction = []
                     data = file.readlines()
                     #TODO chose the block of detail series in reference of the direction
                     #get all the values of the xml file in the first block of detail series
+                    first_block = True
                     for line in data:
                         if re.search(r"</ccma:Detail_Series>", line):
-                            break
-                        if re.search(r"<ccma:quantity>.*?</ccma:quantity>", line):
+                            first_block = False
+                        if re.search(r"<ccma:direction>.*?</ccma:direction>", line):
+                            direction.append(re.search(r"<ccma:direction>(.*?)</ccma:direction>", line).group(1))
+                        if re.search(r"<ccma:quantity>.*?</ccma:quantity>", line) and first_block:
                             quantity.append(re.findall(r'\d+', line)[0])
                     #add the values to the json
-                    json_data[os.path.splitext(file_name)[0]] = ",".join(quantity)
+                    values_xml_name = os.path.splitext(file_name)[0]
+                    json_data[values_xml_name] = ",".join(quantity)
+                    json_data[values_xml_name + '_direction'] = ",".join(direction)
 
         #write the json file
         #json_path = xml_path + 'values.json'
